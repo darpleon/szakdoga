cec60d4588512ddc07d127f120485b5ede59df77
Nátán Sandle <natansandle@gmail.com>
2024-09-20 21:18:29 +0200

    separated function definitions from declarations in Polynomial.hpp

--- src/Polynomial.hpp
+++ src/Polynomial.hpp
@@ -25,72 +25,100 @@ template <typename coeff_type = double>
 class Polynomial
 {
 public:
-    Polynomial()
-    {}
+    Polynomial();
 
-    Polynomial(PowerPermutation pp)
-    {
-        terms[pp] = one<coeff_type>();
-    }
+    Polynomial(const PowerPermutation& pp);
 
-    Polynomial(std::initializer_list<std::pair<const PowerPermutation, coeff_type>> init_list) :
-        terms{init_list}
-    {}
+    Polynomial(std::initializer_list<std::pair<const PowerPermutation, coeff_type>> init_list);
 
-    const std::unordered_map<PowerPermutation, coeff_type>& get_terms() const
-    {
-        return terms;
-    }
+    const std::unordered_map<PowerPermutation, coeff_type>& get_terms() const;
 
-    Polynomial operator+(const Polynomial& p) const
-    {
-        Polynomial result{*this};
-        for (const auto& [permutation, coeff] : p.get_terms()) {
-            if (terms.contains(permutation)) {
-                result.terms[permutation] += coeff;
-            }
-            else {
-                result.terms[permutation] = coeff;
-            }
-        }
-        return result;
-    }
+    Polynomial operator+(const Polynomial& p) const;
 
-    Polynomial operator-() const
-    {
-        Polynomial result;
-        for (const auto& [permutation, coeff] : terms) {
-            result.terms[permutation] = -coeff;
-        }
-    }
+    Polynomial operator-() const;
 
     template <typename input_type = double>
-    input_type operator()(std::initializer_list<std::pair<const Variable, input_type>> init_list) {
-        std::unordered_map<Variable, std::vector<input_type>> values;
-        for (auto [variable, value] : init_list) {
-            values[variable].push_back(one<input_type>());
-            values[variable].push_back(value);
-        }
-        input_type result = zero<input_type>();
-        for (const auto& [permutation, coeff] : terms) {
-            input_type product = one<input_type>();
-            for (auto [variable, power] : permutation.get_monomials()) {
-                while (power >= values[variable].size()) {
-                    values[variable].push_back(values[variable].back() * values[variable][1]);
-                }
-                product *= values[variable][power];
-            }
-            result += product * coeff;
-        }
-        return result;
-    }
-
+    input_type operator()(std::initializer_list<std::pair<const Variable, input_type>> init_list) const;
 private:
     std::unordered_map<PowerPermutation, coeff_type> terms;
 };
 
 template <typename coeff_type>
-inline std::ostream& operator<<(std::ostream& os, Polynomial<coeff_type> p)
+std::ostream& operator<<(std::ostream& os, Polynomial<coeff_type> p);
+
+template <typename coeff_type>
+Polynomial<coeff_type> operator*(const PowerPermutation& pp, coeff_type coeff);
+
+template <typename coeff_type>
+Polynomial<coeff_type>::Polynomial()
+{}
+
+template <typename coeff_type>
+Polynomial<coeff_type>::Polynomial(const PowerPermutation& pp)
+{
+    terms[pp] = one<coeff_type>();
+}
+
+template <typename coeff_type>
+Polynomial<coeff_type>::Polynomial(std::initializer_list<std::pair<const PowerPermutation, coeff_type>> init_list) :
+    terms{init_list}
+{}
+
+template <typename coeff_type>
+const std::unordered_map<PowerPermutation, coeff_type>& Polynomial<coeff_type>::get_terms() const
+{
+    return terms;
+}
+
+template <typename coeff_type>
+Polynomial<coeff_type> Polynomial<coeff_type>::operator+(const Polynomial& p) const
+{
+    Polynomial result{*this};
+    for (const auto& [permutation, coeff] : p.get_terms()) {
+        if (terms.contains(permutation)) {
+            result.terms[permutation] += coeff;
+        }
+        else {
+            result.terms[permutation] = coeff;
+        }
+    }
+    return result;
+}
+
+template <typename coeff_type>
+Polynomial<coeff_type> Polynomial<coeff_type>::operator-() const
+{
+    Polynomial result;
+    for (const auto& [permutation, coeff] : terms) {
+        result.terms[permutation] = -coeff;
+    }
+}
+
+template <typename coeff_type>
+template <typename input_type>
+input_type Polynomial<coeff_type>::operator()(std::initializer_list<std::pair<const Variable, input_type>> init_list) const
+{
+    std::unordered_map<Variable, std::vector<input_type>> values;
+    for (auto [variable, value] : init_list) {
+        values[variable].push_back(one<input_type>());
+        values[variable].push_back(value);
+    }
+    input_type result = zero<input_type>();
+    for (const auto& [permutation, coeff] : terms) {
+        input_type product = one<input_type>();
+        for (auto [variable, power] : permutation.get_monomials()) {
+            while (power >= values[variable].size()) {
+                values[variable].push_back(values[variable].back() * values[variable][1]);
+            }
+            product *= values[variable][power];
+        }
+        result += product * coeff;
+    }
+    return result;
+}
+
+template <typename coeff_type>
+std::ostream& operator<<(std::ostream& os, Polynomial<coeff_type> p)
 {
     bool first = true;
     for (const auto& [pp, coeff] : p.get_terms()) {
